afe6d45038745063a90778ee0d02527757db3e54
Ismoil Nosr <ismoil.nosr@gmail.com>
2020-12-06 00:30:17 +0500

    docker test

--- initDB.js
+++ initDB.js
@@ -7,8 +7,8 @@ module.exports = () => {
 
     connect(host, {
         dbName: process.env.DB_NAME,
-        // user: process.env.DB_NAME,
-        // pass: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASSWORD,
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: true
